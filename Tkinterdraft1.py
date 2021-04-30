572039e580f4b0b339b704f1a69e4ec134b66c3c
max576 <ti20726@bristol.ac.uk>
2021-04-30 20:30:23 +0100

    Got it working! (ish)

--- Tkinterdraft1.py
+++ Tkinterdraft1.py
@@ -9,28 +9,39 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Covid-19 Simulator")
+window.columnconfigure([0, 1], minsize=150)
+window.rowconfigure([0, 1, 2], minsize=50)
+window.resizable(width=False, height=False)
 
-frm_greeting=tk.Frame(master=window)
-lbl_greeting = tk.Label(master=frm_greetingw, text="Hello and welcome to our covid 19 simulator!")
+# frm_greeting=tk.Frame(master=window)
+lbl_greeting = tk.Label(master=window, text="Hello and welcome to our covid 19 simulator!")
 
-frm_info=tk.Frame(master=window)
-lbl_info = tk.Label(master=frm_info, text="This simulator will put you in the position of the PM during the pandemic and test your abilities to run the country.")
+# frm_info=tk.Frame(master=window)
+lbl_info = tk.Label(master=window, text="This simulator will put you in the position of the PM during the pandemic and test your abilities to run the country.")
 
+def _continue():
+    print("Hello world")
+    
 btn_continue = tk.Button(
     master=window,
     text="Continue",
-    command=print("Hello World")
+    command= _continue()
 )
+
+def _quit():
+        window.quit()     # stops mainloop
+        # window.destroy()
+        print("Goodbye!")
 
 btn_quit = tk.Button(
     master=window,
     text="Quit",
-    command=print("Goodbye!"
+    command= _quit()
 )
 
-frm_greeting.grid(row=0, column=0, padx=10)
-frm_info.grid(row=1, column=0, padx=10)
+lbl_greeting.grid(row=0, column=0, padx=10)
+lbl_info.grid(row=1, column=0, padx=10)
 btn_continue.grid(row=2, column=0, padx=10, pady=10)
-btn_quit.grid(row=2, column=1 padx=10, pady=10)
+btn_quit.grid(row=2, column=1, padx=10, pady=10)
 
 window.mainloop()
