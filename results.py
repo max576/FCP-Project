4df8ca68165f320fc123c8249a3c36efdc74a231
max576 <ti20726@bristol.ac.uk>
2021-05-05 18:08:31 +0100

    Added welcome and results screens as seperate scripts to be called by main

---
+++ results.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 13:59:55 2021
+
+@author: max
+"""
+
+import tkinter as tk
+import PIL
+from PIL import ImageTk, Image
+
+results_window = tk.Tk() 
+
+results_window.title("COVID Simulator")
+# results_window.tk.call('wm', 'iconphoto', results_window._w, tk.PhotoImage(file='icon.png'))
+results_window.geometry('1200x1000')
+results_frame1 = tk.Frame(master=results_window, width=50, height=50, bg="red")
+results_frame1.pack(fill=tk.BOTH)
+
+results_label1 = tk.Label(master=results_frame1, bg="red", fg="white", text = "Thank you for using our covid 19 simulator") 
+results_label1.pack()
+
+results_frame2 = tk.Frame(master=results_window, width=50, height=50, bg="white")
+results_frame2.pack()
+
+results_label2 = tk.Label(master=results_frame2, text="Results" + "\n\n blah blah blah blah blah blah blah blah\nblah blah blah blah blah blah blah blah\nblah blah blah blah blah blah blah blah\nblah blah blah blah blah blah blah blah\nblah blah blah blah blah blah blah blah\nblah blah blah blah blah blah blah blah\n")
+results_label2.pack()
+
+results_frame3 = tk.Frame(master=results_window, width=50, height=50,)
+results_frame3.pack()
+
+image1 = Image.open("results.jpg")
+test = ImageTk.PhotoImage(image1)
+label1 = tk.Label(master = results_frame3, image = test)
+label1.image = test
+# Position image
+# label1.place(x=1, y=2)
+
+tk.Button(results_window, text="Quit", command=results_window.destroy, fg="dark green", bg = "white").pack() 
+#display welcome_window, destroyed by button press above
+results_window.mainloop() 
+
+# from PIL import ImageTk,Image.
+# window = tk.Tk()
+# canvas = tk.Canvas(window, width = 300, height = 300)
+# canvas.pack()
+# imageFile = "Aaron.jpg"
+
+# window.im1 = Image.open(imageFile)
+
+
+# raw_input()
+# img = tk.Image(Image.open('results.png'))
+# canvas.create_image(20, 20, anchor=NW, image=img)
+# window.mainloop()
+
+# import tkinter
+# from tkinter import *
+# from PIL import Image, ImageTk
+# root = Tk()
+# # Create a photoimage object of the image in the path
+# image1 = Image.open("results.jpg")
+# test = ImageTk.PhotoImage(image1)
+# label1 = tkinter.Label(image=test)
+# label1.image = test
+# # Position image
+# label1.place(x=1, y=2)
+# root.mainloop()
+
