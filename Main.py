10ef7b3dda1e71efa326a01803ef0bccfcdd80f6
max576 <78923378+max576@users.noreply.github.com>
2021-05-14 13:00:01 +0100

    Added comments

--- Main.py
+++ Main.py
@@ -36,10 +36,10 @@ class epidemic_control:
         self.weight = weight
         self.cost = cost
 
-
+#Controls consisting of the self functions defined above
 def initialize_controls(controls):
     
-    ###IMPROVEMENT - provide csv initialize option for control list parameters
+    #initialize option for control list parameters
     controls.append(epidemic_control(0,"Distancing & Facemasks","Although this is the most basic measure, it is one of the most effective\n at inhibiting the spread of disease. However, it may affect people's mental wellbeing as you're unable\n to hug your loved ones and it may affect the economy as shops and restaurants are unlikely to do as well\n if fewer people are allowed in them as they must be spread further apart.\n ",DISTANCING,[0.6,0.7,0.9,0.9,0.8,0.6], 3.9))
     controls.append(epidemic_control(1,"Early Closing","This will significantly affect the economy as there won't be as much revenue coming\n in to the pubs. Also, the average British person won't be too happy about this.\n\n\n" ,PUBS,[1.7,1.7,1.7,1.7,1.7,1.7], 1.9))
     controls.append(epidemic_control(2,"Close Non-essential Shops","Shops play a huge role in the country's economy - if they were to close,\n many people would lose their jobs and have to go on furlough, and the government would suffer, as\n would the mental wellbeing of the people who no longer have a day job, and some independent shops may have\n to close permanently due to the lack of income. However, indoor spaces are a breeding ground for\n viruses like Covid-19. ",NON_ESSENTIAL,[0.7,0.7,0.9,0.9,0.8,0.7], 16))
@@ -56,11 +56,10 @@ class period:
         self.name = name
         self.story = story
 
-        
+#Periods         
 def initialize_periods(periods):
 
-    ###IMPROVEMENT - provide csv import option for period content
-
+    #import option for period content consisting of the self functions defined above
     periods.append(period(0, 3.2,"Period 1 - January and February","The Health Secretary has informed you that the initial R-Rate is 3.2\n\n Of course, it is very cold throughout January and February and people are naturally coming down\n with the flu over winter. As a rule, spending is low across the UK in retail, but the public still desire\n eating out and drinking in cosy pubs. Although, it is proven that keeping pubs open has a direct\n correlation against the spread of virus. The health secretary advises against keeping cosy pubs open.\n\n"))
     periods.append(period(1, 2.7,"Period 2 - March and April","The Health Secretary has informed you that the new R-Rate is 2.7\n\nThere are tens of thousands of young people currently travelling around South East Asia in hope to\n ‘find themselves’, where there is another variant of a similar virus here in the UK spreading\n simultaneously. A national lockdown has been advised as the spread of the virus has gone\n worldwide, although this is due to have severe economic downfall, and also cause a mental health crisis.\n\n"))
     periods.append(period(2, 2.1,"Period 3 - May and June","The Health Secretary has informed you that the new R-Rate is 2.1\n\nAs the summer months are closely approaching, temperatures are rising to a point\n where the virus is less transmittable. Many students and young people have upcoming\n exams, which they are severely stressed about. Not only have they left\n revision to last minute, but they are also uncertain on whether exams will be happening at all.\n\n"))
@@ -68,7 +67,7 @@ def initialize_periods(periods):
     periods.append(period(4, 2.0,"Period 5 - September and October","The Health Secretary has informed you that the new R-Rate is 2.0\n\n As summer closes to an end, the new academic year is looming over us. Students\n and staff seek security and certainty for the forthcoming months, as the previous academic\n year had been meddled with by the scare and anxiety of the virus, and different parameters\n put into place in a bid to reduce the R-rate.\n\n"))
     periods.append(period(5, 2.9,"Period 6 - November and December","The Health Secretary has informed you that the new R-Rate is 2.9\n\n As we edge into Winter, a sharp bite in dropping temperatures swarm the UK, giving the virus\n a stronger environment to spread and mutate in. Arriving close to December, retail and hospitality\n face the busiest periods of the year, in hope of capitalising on what has been a terrible year\n otherwise. \n\n"))
 
-
+#Functions for hover feature - activates when mouse is over each checkbox and chnages depending on which control
 def on_enter(event):
     num = str(event.widget)[-1]
     if num == "n": num = "1"
@@ -77,11 +76,11 @@ def on_enter(event):
     pPrompt.set(ctl[num].prompt)
     label4.configure(textvariable=pPrompt)
 
-
+#Resets label to blank when mouse leaves checkbox
 def on_leave(event):
     label4.configure(text="")    
     
-
+#function that plots the graph and changes it for each period
 def plot_and_move_next_period(fig, canvas, cb_var):
    #plot chart for this period
     n = 0
@@ -97,10 +96,8 @@ def plot_and_move_next_period(fig, canvas, cb_var):
     plot(fig, canvas, userchoice)
 
         
-    #final period? disable button otherwese set next period and  load  next period instructions
+    #Chnages "next period2 button to "results" and changes the command to open the reults page
     if current_period.get() == len(periods)-1 : 
-        # next_button["text"] = "Results"
-        # next_button["state"] = "enabled"
         next_button.configure(text = "Results", command = show_results)
 
 
@@ -108,15 +105,15 @@ def plot_and_move_next_period(fig, canvas, cb_var):
         current_period.set(current_period.get() + 1)
         pName.set(periods[current_period.get()].name + "\n\n" + periods[current_period.get()].story)
           
-    
+#gets info from the tkinter window on the current period    
 def currPeriod(event=None):
     return current_period.get()
 
-
+#initialises magic nuber (initial infections)
 def magic_number():
     return MAGIC_NUMBER
 
-
+#Function that plots the graph
 def plot(fig, canvas, userchoice):
 
     #Instanticate list for periods (x) , and player guess(y) and actuals(y2)
@@ -183,22 +180,13 @@ def plot(fig, canvas, userchoice):
     plot1.set_title('Death Toll after parameter ')
 
 
-    # plot1.plot(x, y,  'b', alpha=0.5, lw=2, label = "Your Guess")
-    # plot1.plot(x, y2,  'g', alpha=0.5, lw=2, label = "Unhinged")
-
-    # plot1.legend(['Your Guess','Unhinged']) 
-    # plot1.set_xlabel('Period')
-    # plot1.set_ylabel('Death toll (people)')
-    # plot1.grid(b=True, which='major', c='w', lw=2, ls='-')
-    # plot1.set_title('Death Toll after parameter ')
-
-
 	# creating the Tkinter canvas
 	# containing the Matplotlib figure
 
     canvas.draw()
     canvas.get_tk_widget().pack()
     
+    #overrides the fig in PLOTS folder to be displayed in the results screen
     if currPeriod() >= 0: fig.savefig('PLOTS/death.png')
     if currPeriod() > 0: plot1.remove()
     if currPeriod() > 0: del plot1
@@ -222,7 +210,8 @@ def calc_pie():
            uc += 1  
        costs1.append(e)    
        p += 1
-    
+       
+    #If user does not select anything a message will pop up telling them to restart
     if int(e) == 0:
         fig, ax = plt.subplots()
         controls = ['']
@@ -233,7 +222,8 @@ def calc_pie():
         plt.title("Total Debt: £0\n\n You did not implement any control measures.\n\n  Please restart to get full use out of this simulator.", bbox={'facecolor':'0.8', 'pad':5})
         fig.tight_layout()
         plt.savefig("PLOTS/pie.png")    
-        
+     
+    #othereise pie chart of debt will be produced    
     if int(e) > 0:
         fig, ax = plt.subplots()
         fig = plt.Figure(figsize=(7.5,5), dpi=100)
@@ -248,19 +238,19 @@ def calc_pie():
         plt.savefig("PLOTS/pie.png")
         #plt.show()
     
-    
+#function that calls help window if user is stuck
 def Show_help():
     from SCRIPTS._help import Get_help
     Get_help()
     
-    
+#function calls results screen    
 def show_results():
     calc_pie()
     window.destroy()
     from SCRIPTS.results import display_results
     display_results()
     
-    
+#sets up replay button for results screen
 def Replay():
     from SCRIPTS.welcome import display_welcome_screen
     display_welcome_screen()
